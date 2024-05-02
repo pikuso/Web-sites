ef036730efc93b8b121b43b2dd27c0caf28ca0cb
Polina Ponezha <87444866+pikuso@users.noreply.github.com>
2024-05-02 16:20:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 const textBox = document.getElementById("textBox");
 const toFarthenheit = document.getElementById("toFarthenheit");
-const toCelsium = document.getElementById("toCelsium");
+const toCelsius = document.getElementById("toCelsius");
 const result = document.getElementById("result");
 let temp;
 
 function convert() {
   if (toFarthenheit.checked) {
-   // result.textContent = "You selected to Fahrenheit";
    temp = Number(textBox.value);
    temp = temp * 9 / 5 + 32;
-   result.textContent = temp.toFixed(1) + "°F"
+   result.textContent = temp.toFixed(1) + "°F";
 
-  } else if (toCelsium.checked) {
-    //result.textContent = "You selected to Celsium";
+  } else if (toCelsius.checked) {
     temp = Number(textBox.value);
     temp = (temp - 32) * (5 / 9);
-    result.textContent = temp.toFixed(1) + "°С"
+    result.textContent = temp.toFixed(1) + "°C";
   } else {
     result.textContent = "Select a unit";
   }
